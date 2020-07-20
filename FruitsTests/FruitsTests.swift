4f53492aded6b7621b7bfe9c2c33761b0cd2511f
Adam Rush <adam.rush2@skybettingandgaming.com>
2020-07-20 09:18:42 +0100

    Update project settings and fixing SwiftLint indentations.

--- FruitsTests/FruitsTests.swift
+++ FruitsTests/FruitsTests.swift
@@ -33,17 +33,16 @@
 import XCTest
 
 class FruitsTests: XCTestCase {
+  override func setUpWithError() throws {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+	override func tearDownWithError() throws {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+	func testExample() throws {
+		// This is an example of a functional test case.
+		// Use XCTAssert and related functions to verify your tests produce the correct results.
+	}
 }
